de905b61465e46a0c5bf6524ab723d03c70128f1
Olivia Coumans <livie7c@gmail.com>
2021-12-30 11:43:12 +0100

    feat(styles): update global colors

--- web/styles/GlobalStyles.tsx
+++ web/styles/GlobalStyles.tsx
@@ -2,17 +2,14 @@ import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   :root {
-    --bg-color: hsl(230deg, 33%, 21%);
-    --text-color: hsl(230deg, 57%, 90%);
+    --bg-color: #fffffe;
+    --text-color: #2b2c34;
+    --link-color: #6246ea;
 
-    --heading-color: hsl(60deg, 100%, 100%);
-    --text-color: hsl(230deg, 57%, 90%);
-    --dark-text-color: hsl(230deg, 39%, 12%);
+    --button-text: #fffffe;
+    --button-bg: #6246ea;
 
-    --button-text: hsl(230deg, 33%, 21%);
-    --button-bg: hsl(351deg, 61%, 89%);
-
-    --page-padding: 32px
+    --page-padding: 32px;
   }
 
   html,
@@ -34,7 +31,7 @@ const GlobalStyles = createGlobalStyle`
   body {
     background-color: var(--bg-color);
     color: var(--text-color);
-    font-family: 'Inter', sans-serif;
+    font-family: 'Merriweather', serif;
     line-height: 1.5;
   }
 `;
