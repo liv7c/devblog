305382fb4860c2e8fc803221f7213c1ea527b388
Olivia Coumans <livie7c@gmail.com>
2022-01-03 11:08:56 +0100

    chore(config): update url config

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev
-  ? 'http://localhost:3000'
-  : 'https://admiring-austin-d06378.netlify.app';
+export const server = dev ? 'http://localhost:3000' : 'https://oliviac.dev';
