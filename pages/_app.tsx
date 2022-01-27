263f9669877e6d975a0eca8168f9680d1b33e937
Olivia Coumans <livie7c@gmail.com>
2022-01-27 08:17:45 +0100

    feat(app): use fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,35 @@
 import 'normalize.css';
 
-import type {AppProps} from 'next/app';
+import * as Fathom from 'fathom-client';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import GlobalStyles from '../styles/GlobalStyles';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('RPEVBUFY', {
+      includedDomains: ['oliviac.dev', 'www.oliviac.dev'],
+      url: 'https://encouraging-descriptive.oliviac.dev/script.js',
+      honorDNT: true,
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <GlobalStyles />
