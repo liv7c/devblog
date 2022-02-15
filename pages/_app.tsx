8a77fe230fabda8e3f3f4d9e28a4bcde264a0384
Olivia Coumans <livie7c@gmail.com>
2022-02-15 09:02:24 +0100

    chore: clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import 'normalize.css';
 
-import * as Fathom from 'fathom-client';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -9,26 +8,6 @@ import GlobalStyles from '../styles/GlobalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
-  useEffect(() => {
-    Fathom.load('RPEVBUFY', {
-      includedDomains: ['oliviac.dev', 'www.oliviac.dev'],
-      url: 'https://encouraging-descriptive.oliviac.dev/script.js',
-      honorDNT: true,
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, []);
 
   return (
     <>
