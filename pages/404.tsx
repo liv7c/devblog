9c805a7f7782a41b3e33a3c89be04950e0125256
Olivia Coumans <livie7c@gmail.com>
2022-02-19 11:17:27 +0100

    chore(404): remove unused import

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import Link from 'next/link';
 
 import Layout from '../components/Layout';
-import { PageTitleWrapper } from '../styles/Wrapper';
 
 const NotFoundPage: NextPage = () => {
   return (
