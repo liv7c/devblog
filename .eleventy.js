ae6118a403e1ee4ee5c727660fe68f2c61d09e5e
Olivia Coumans <livie7c@gmail.com>
2022-02-27 16:14:05 +0100

    feat(blog): add blogs posts with css

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
+const {DateTime} = require('luxon');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addWatchTarget('./src/sass/');
   eleventyConfig.addPassthroughCopy('./src/css/');
 
   eleventyConfig.addWatchTarget('./src/js/');
 
   eleventyConfig.addNunjucksGlobal('currentYear', new Date().getFullYear());
+
+  eleventyConfig.addFilter('asPostDate', (date) => {
+    return DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED);
+  });
+
+  eleventyConfig.addFilter('isoDate', (date) => {
+    return DateTime.fromJSDate(date).toISODate();
+  });
+
+  eleventyConfig.addFilter('limit', (arr, limit) => arr.slice(0, limit));
 
   return {
     dir: {
