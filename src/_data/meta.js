be970907abce553c634ed3a294d63793a93ceb8f
Olivia Coumans <livie7c@gmail.com>
2022-02-27 17:44:05 +0100

    feat(seo): basic seo

---
+++ src/_data/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url: process.env.URL || 'http://localhost:8080',
+  siteName: 'Olivia Coumans',
+  siteDescription: 'Olivia Coumans, front-end developer in Paris',
+  authorName: 'Olivia Coumans',
+  twitterUsername: 'liv_codes', // no `@`
+};
