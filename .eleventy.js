917ee9f36ce3343be52c4da9c929b84503d217c1
Olivia Coumans <livie7c@gmail.com>
2022-02-27 18:02:07 +0100

    feat(rss): set up rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const {DateTime} = require('luxon');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addWatchTarget('./src/sass/');
   eleventyConfig.addPassthroughCopy('./src/css/');
