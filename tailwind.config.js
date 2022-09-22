132d3bbbed176cf5ea23987d1de5a5f57dccbbfd
Olivia Coumans <livie7c@gmail.com>
2022-09-22 04:44:03 +0200

    chore(remix): init remix app

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx,jsx,js}'],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#fffffe',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#33272a',
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
