b05975cd5a2db2290f9f5f150aa62a0473e2cb92
Olivia Coumans <livie7c@gmail.com>
2022-09-22 04:44:03 +0200

    chore(remix): init remix app

---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <main className="prose bg-beige">
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nostrum
+        suscipit in dolores omnis, modi adipisci corporis magnam expedita
+        deleniti veniam nemo at placeat veritatis maiores repellendus quibusdam
+        facilis blanditiis.
+      </p>
+    </main>
+  );
+}
