1bbada2df374fbb37a2dff23fbd02f86da935731
Olivia Coumans <livie7c@gmail.com>
2022-10-20 14:52:47 +0200

    fix(seo): fix blog img

--- app/utils/seo.ts
+++ app/utils/seo.ts
@@ -17,16 +17,16 @@ export function getSEOMeta({
   return {
     description,
     keywords,
-    image: '/img/blog-img.png',
+    image: 'https://oliviac.dev/img/blog-img.png',
     'og:url': url,
     'og:title': title,
     'og:description': description,
-    'og:image': '/img/blog-img.png',
+    'og:image': 'https://oliviac.dev/img/blog-img.png',
     'twitter:card': 'summary',
     'twitter:creator': '@liv_codes',
     'twitter:title': title,
     'twitter:description': description,
-    'twitter:image': '/img/blog-img.png',
+    'twitter:image': 'https://oliviac.dev/img/blog-img.png',
     'twitter:alt': title,
   };
 }
