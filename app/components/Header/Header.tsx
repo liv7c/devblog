866de47bc8bc67c24cd8cc5067108cf9201ce7ff
Olivia Coumans <livie7c@gmail.com>
2022-11-13 09:36:43 +0100

    refactor(header): remove mobile menu

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,20 +1,9 @@
 import type {ReactNode} from 'react';
-import {useEffect, useState} from 'react';
-import {Link, NavLink, useLocation} from '@remix-run/react';
+import {Link, NavLink} from '@remix-run/react';
 import clsx from 'clsx';
 
-const MenuLink = ({
-  to,
-  children,
-  isLast = false,
-}: {
-  to: string;
-  children: ReactNode;
-  isLast?: boolean;
-}) => {
-  const classes = clsx('block py-3 px-4 no-underline text-lg', {
-    'mr-4': !isLast,
-  });
+const MenuLink = ({to, children}: {to: string; children: ReactNode}) => {
+  const classes = clsx('block py-3 px-4 no-underline text-lg');
 
   return (
     <NavLink to={to} className={classes} prefetch="intent">
@@ -24,54 +13,29 @@ const MenuLink = ({
 };
 
 function Header() {
-  const [menuVisible, setMenuVisible] = useState(false);
-  const {pathname} = useLocation();
-
-  const toggleMenu = () => {
-    setMenuVisible((prevState) => !prevState);
-  };
-
-  useEffect(() => {
-    setMenuVisible(false);
-  }, [pathname]);
-
   return (
     <header>
       <a href="#main-content" className="skip-link">
         Skip to main content
       </a>
-      <div className="flex justify-between container items-center md:items-baseline h-full relative">
-        <span>
-          <Link to="/" className="no-underline text-lg">
-            Olivia Coumans
-          </Link>
-        </span>
-        <nav aria-label="Main" className="h-full">
-          <button
-            className="md:hidden bg-slate-700 text-white py-3 px-4 rounded mt-2 mb-2"
-            onClick={toggleMenu}
-            aria-expanded={menuVisible}
-          >
-            Menu
-          </button>
+      <div className="pt-2 md:py-0 flex flex-col md:flex-row items-center justify-between container md:items-baseline h-full relative">
+        <Link to="/" className="block pt-2 pb-1 no-underline text-lg font-bold">
+          Olivia Coumans
+        </Link>
+        <nav aria-label="Main" className="h-full mt-1 md:mt-0">
           <ul
             className={clsx(
-              'list-none p-0 m-0 md:flex items-baseline absolute z-40 w-screen md:w-auto md:static left-0 mt-6 md:mt-0 bg-beige md:bg-transparent',
-              {
-                hidden: !menuVisible,
-              }
+              'list-none px-0 m-0 flex justify-center items-baseline space-x-2'
             )}
           >
-            <li className="mb-2 md:mb-0 border-b border-gray-200 md:border-none">
+            <li className="-my-1 md:my-2">
               <MenuLink to="/">Home</MenuLink>
             </li>
-            <li className="mb-2 md:mb-0 border-b border-gray-200 md:border-none">
+            <li className="-my-1 md:my-2">
               <MenuLink to="/blog">Blog</MenuLink>
             </li>
-            <li className="border-b border-gray-200 md:border-none">
-              <MenuLink to="/about" isLast>
-                About
-              </MenuLink>
+            <li className="-my-1 md:my-2">
+              <MenuLink to="/about">About</MenuLink>
             </li>
           </ul>
         </nav>
