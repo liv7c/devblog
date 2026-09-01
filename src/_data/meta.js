01c854d54aab9feb1ab2f40473b5b66facd9f004
Olivia Coumans <livie7c@gmail.com>
2026-09-01 06:18:50 -0400

    feat: update meta desc and location details

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,6 +1,7 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
   siteName: 'Olivia Coumans',
-  siteDescription: 'Olivia Coumans, front-end developer in Paris',
+  siteDescription:
+    'Olivia Coumans, front-end developer in Toronto, Canada. React, TypeScript, web accessibility, and testing.',
   authorName: 'Olivia Coumans',
 };
